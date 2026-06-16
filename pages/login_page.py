6ab6d35e38f07826d2be1598c04fcd4c53b95a27
SteffiVinod5 <57933093+SteffiVinod5@users.noreply.github.com>
2026-06-16 22:27:36 +0530

    Fix login button locator

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,7 +3,7 @@ class LoginPage:
         self.page = page
         self.username = page.locator("#loginId")
         self.password = page.locator("input[type=\"password\"]")
-        self.login_button = page.locator(".d-flex")
+        self.login_button = page.get_by_role("button", name="Login")
 
     def open(self):
         self.page.goto("https://pwa.skordev.com/#/login")
@@ -11,5 +11,11 @@ class LoginPage:
     def login(self, user_name, password):
         self.username.fill(user_name)
         self.password.fill(password)
+
+        self.password.press("Tab")
+        self.page.wait_for_timeout(2000)
+
+        print("Login button enabled:", self.login_button.is_enabled())
+
         self.login_button.click()
         self.page.wait_for_timeout(3000)
